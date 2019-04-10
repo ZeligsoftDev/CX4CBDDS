164922938de5c4bee0122fc0af1a01af413a1b90
Tuhin Kanti Das <tuhin@zeligsoft.com>
2019-04-10 15:49:55 -0400

    regenerated concepts constants for CCM

--- bundles/com.zeligsoft.domain.omg.ccm/src/com/zeligsoft/domain/omg/ccm/CCMNames.java
+++ bundles/com.zeligsoft.domain.omg.ccm/src/com/zeligsoft/domain/omg/ccm/CCMNames.java
@@ -6,7 +6,7 @@
  * You may obtain a copy of the License at
  *
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+ *  
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -459,13 +459,6 @@ public final class CCMNames {
 	 * @generated
 	 */
 	public static final String INTERFACE_PORT__IS_ASYNCHRONOUS = "isAsynchronous";//$NON-NLS-1$  
-
-	/**
-	 * ZDL DomainReference: InterfacePort::testAttr. 
-	 * Lower: 0 Upper: 1 
-	 * @generated
-	 */
-	public static final String INTERFACE_PORT__TEST_ATTR = "testAttr";//$NON-NLS-1$ 
 
 	/**
 	 * Fully qualified name for the ZDL DomainConcept: Manages. 
