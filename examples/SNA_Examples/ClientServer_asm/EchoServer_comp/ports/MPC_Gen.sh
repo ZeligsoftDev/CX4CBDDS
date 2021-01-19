d04b42c72e2155c939a7e7a6cfa54709b8fc7349
Young-Soo Roh <ysroh@zeligsoft.com>
2021-01-19 16:59:17 -0500

    Example migration and clean up

--- examples/SNA_Examples/ClientServer_asm/EchoServer_comp/ports/MPC_Gen.sh
+++
@@ -1,6 +0,0 @@
-sna_generate_mpc.py \
-SNA_Examples_EchoConnect_obj.idl
-
-if [ $? -ne 0 ]; then
-   exit 1
-fi
--- examples/SNA_Examples/ExternalAdapter_asm/RelayServer_comp/ports/MPC_Gen.sh
+++
@@ -1,5 +0,0 @@
-sna_generate_mpc.py SNA_Examples_RelayServer_obj.idl
-
-if [ $? -ne 0 ]; then
-   exit 1
-fi
