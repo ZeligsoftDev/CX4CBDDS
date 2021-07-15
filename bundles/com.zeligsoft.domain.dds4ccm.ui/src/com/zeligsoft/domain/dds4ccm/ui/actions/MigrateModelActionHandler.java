3c4e10b36a4e8661a281736414b2dafa9966cedd
Young-Soo Roh <ysroh@zeligsoft.com>
2021-07-15 09:34:30 -0400

    Issue #224: Create all diagrams that require diagram migration

--- bundles/com.zeligsoft.domain.dds4ccm.ui/src/com/zeligsoft/domain/dds4ccm/ui/actions/MigrateModelActionHandler.java
+++ bundles/com.zeligsoft.domain.dds4ccm.ui/src/com/zeligsoft/domain/dds4ccm/ui/actions/MigrateModelActionHandler.java
@@ -16,6 +16,9 @@
  */
 package com.zeligsoft.domain.dds4ccm.ui.actions;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
@@ -24,20 +27,27 @@ import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.emf.common.command.Command;
+import org.eclipse.emf.common.util.TreeIterator;
+import org.eclipse.emf.ecore.EAnnotation;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.transaction.TransactionalEditingDomain;
 import org.eclipse.emf.transaction.util.TransactionUtil;
 import org.eclipse.gmf.runtime.common.core.command.CommandResult;
 import org.eclipse.gmf.runtime.emf.commands.core.command.AbstractTransactionalCommand;
+import org.eclipse.gmf.runtime.notation.Diagram;
 import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.papyrus.infra.core.resource.ModelSet;
 import org.eclipse.papyrus.infra.emf.gmf.command.GMFtoEMFCommandWrapper;
+import org.eclipse.papyrus.uml.diagram.composite.CreateCompositeDiagramCommand;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.progress.UIJob;
+import org.eclipse.uml2.uml.Component;
 import org.eclipse.uml2.uml.Model;
 
 import com.zeligsoft.base.ui.utils.BaseUIUtil;
 import com.zeligsoft.domain.dds4ccm.ui.l10n.Messages;
 import com.zeligsoft.domain.dds4ccm.utils.DDS4CCMMigrationUtil;
+import com.zeligsoft.domain.omg.ccm.util.CCMUtil;
 
 /**
  * Migrate DDS4CCM model
@@ -56,6 +66,8 @@ public class MigrateModelActionHandler extends AbstractHandler {
 
 		if (DDS4CCMMigrationUtil.isMigrationRequired((Model) selObject)) {
 
+			List<Component> assemblies = getAssembliesWithRSADiagramAnnotation(selObject);
+
 			TransactionalEditingDomain editingDomain = TransactionUtil.getEditingDomain(selObject);
 			final AbstractTransactionalCommand migrationCommand = new AbstractTransactionalCommand(
 					TransactionUtil.getEditingDomain(selObject), Messages.MigrateModelAction_CommandLabel, null) {
@@ -65,6 +77,13 @@ public class MigrateModelActionHandler extends AbstractHandler {
 						throws ExecutionException {
 
 					DDS4CCMMigrationUtil.migrateAll((Model) selObject);
+					// create structure diagrams
+					for (Component assembly : assemblies) {
+						CreateCompositeDiagramCommand cmd = new CreateCompositeDiagramCommand();
+						cmd.createDiagram((ModelSet) selObject.eResource().getResourceSet(), assembly,
+								assembly.getName() + " StructureDiagram"); //$NON-NLS-1$
+					}
+
 					return CommandResult.newOKCommandResult();
 				}
 			};
@@ -90,5 +109,30 @@ public class MigrateModelActionHandler extends AbstractHandler {
 					Messages.MigrateModelAction_DialogTitle, Messages.Migrate_Noop);
 		}
 		return null;
+	}
+	
+	
+	/**
+	 * Get assemblies that contains RSA diagram layout information
+	 * 
+	 * @param model
+	 * @return
+	 */
+	public static List<Component> getAssembliesWithRSADiagramAnnotation(EObject model) {
+		final List<Component> result = new ArrayList<Component>();
+		for (TreeIterator<EObject> iter = model.eAllContents(); iter.hasNext();) {
+			EObject next = iter.next();
+
+			if (!(next instanceof org.eclipse.uml2.uml.Package) && !(next instanceof Component)) {
+				iter.prune();
+			} else if (CCMUtil.isAssemblyComponent(next)) {
+				EObject assembly = CCMUtil.getAssemblyFromComponent(next);
+				EAnnotation annot = ((Component) assembly).getEAnnotation("cx.diagrams"); //$NON-NLS-1$
+				if (annot != null && !annot.eContents().isEmpty() && annot.eContents().get(0) instanceof Diagram) {
+					result.add((Component) assembly);
+				}
+			}
+		}
+		return result;
 	}
 }
