babd7f61c018cd674e04e527537e94a75108f4c3
young-soo Roh <ysroh@zeligsoft.com>
2021-11-17 15:07:26 -0500

    Issue #346: Fix refresh issue with assembly diagram when ports are added to the assembly definition

--- bundles/com.zeligsoft.domain.dds4ccm.ui/src/com/zeligsoft/domain/dds4ccm/ui/editpolicies/DDS4CCMCanonicalEditPolicy.java
+++ bundles/com.zeligsoft.domain.dds4ccm.ui/src/com/zeligsoft/domain/dds4ccm/ui/editpolicies/DDS4CCMCanonicalEditPolicy.java
@@ -1,5 +1,5 @@
 /**
- * Copyright 2020 Northrop Grumman Systems Corporation.
+ * Copyright 2020-2021 Northrop Grumman Systems Corporation.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -19,16 +19,23 @@ package com.zeligsoft.domain.dds4ccm.ui.editpolicies;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.eclipse.emf.ecore.EObject;
 import org.eclipse.gmf.runtime.notation.View;
 import org.eclipse.papyrus.infra.gmfdiag.canonical.editpolicy.PapyrusCanonicalEditPolicy;
+import org.eclipse.uml2.uml.Component;
+
+import com.zeligsoft.base.zdl.util.ZDLUtil;
+import com.zeligsoft.domain.omg.ccm.CCMNames;
 
 /**
- * Override Papyrus canonical edit policy to support gmf geoshapes
+ * DDS4CCM canonical edit policy to implement workarounds
  * 
  * @author Young-Soo Roh
  *
  */
 public class DDS4CCMCanonicalEditPolicy extends PapyrusCanonicalEditPolicy {
+
+	private static final String SEMI_ACTIVE_FILTER_ID = "org.eclipse.papyrus.semiCanonical"; //$NON-NLS-1$
 
 	@Override
 	protected boolean deleteViews(Iterable<? extends View> views) {
@@ -39,6 +46,21 @@ public class DDS4CCMCanonicalEditPolicy extends PapyrusCanonicalEditPolicy {
 			}
 		}
 		return super.deleteViews(toDelete);
+	}
+
+	@Override
+	protected void doActivate() {
+		super.doActivate();
+		EObject semanticHost = getSemanticHost();
+
+		// Listen to the component definition as well 
+		if (semanticHost != null && ZDLUtil.isZDLConcept(semanticHost, CCMNames.ASSEMBLY_IMPLEMENTATION)) {
+			Component assembly = (Component) semanticHost;
+			if (!assembly.getGenerals().isEmpty()) {
+				EObject general = ((Component) semanticHost).getGenerals().get(0);
+				addListenerFilter(SEMI_ACTIVE_FILTER_ID, this, general);
+			}
+		}
 	}
 
 }
