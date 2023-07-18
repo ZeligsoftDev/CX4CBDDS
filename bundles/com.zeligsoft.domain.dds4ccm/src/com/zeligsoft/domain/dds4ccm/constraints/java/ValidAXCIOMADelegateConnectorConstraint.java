f9cbf212297e3128e4801652af781f0431db7e8f
Ernesto Posse <ernesto.posse@lumenix.com>
2023-07-18 17:53:33 -0400

    Issue #462: Implementing updated validation rule
    Signed-off-by: Ernesto Posse <ernesto.posse@lumenix.com>

--- bundles/com.zeligsoft.domain.dds4ccm/src/com/zeligsoft/domain/dds4ccm/constraints/java/ValidAXCIOMADelegateConnectorConstraint.java
+++ bundles/com.zeligsoft.domain.dds4ccm/src/com/zeligsoft/domain/dds4ccm/constraints/java/ValidAXCIOMADelegateConnectorConstraint.java
@@ -16,10 +16,31 @@
  */
 package com.zeligsoft.domain.dds4ccm.constraints.java;
 
-import org.eclipse.core.runtime.IStatus;
-import org.eclipse.emf.validation.IValidationContext;
+import java.util.List;
 
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.preferences.IEclipsePreferences;
+import org.eclipse.core.runtime.preferences.InstanceScope;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.validation.IValidationContext;
+import org.eclipse.uml2.uml.Class;
+import org.eclipse.uml2.uml.Component;
+import org.eclipse.uml2.uml.ConnectorEnd;
+import org.eclipse.uml2.uml.Element;
+import org.eclipse.uml2.uml.Model;
+
+import com.zeligsoft.base.zdl.util.ZDLUtil;
+import com.zeligsoft.domain.dds4ccm.Activator;
+import com.zeligsoft.domain.dds4ccm.ConnectorType;
+import com.zeligsoft.domain.dds4ccm.DDS4CCMPreferenceConstants;
+import com.zeligsoft.domain.dds4ccm.utils.ConnectorTypeUtility;
+import com.zeligsoft.domain.idl3plus.IDL3PlusNames;
+import com.zeligsoft.domain.idl3plus.api.Connectors.ConnectorDef;
+import com.zeligsoft.domain.omg.ccm.CCMNames;
 import com.zeligsoft.domain.omg.ccm.constraints.java.ValidDelegateConnectorConstraint;
+import com.zeligsoft.domain.zml.util.ZMLMMNames;
+
+import dds4ccm.ComponentInterface;
 
 /**
  * @author eposse
@@ -33,7 +54,105 @@ public class ValidAXCIOMADelegateConnectorConstraint extends ValidDelegateConnec
 
 	@Override
 	public IStatus validate(IValidationContext ctx) {
-		System.out.println("[DEBUG][EPP] new ValidAXCIOMADelegateConnectorConstraint");
-		return super.validate(ctx);
+
+		EObject objToVerify = ctx.getTarget();
+
+		if (ZDLUtil.isZDLConcept(objToVerify, CCMNames.CCMCONNECTOR)) {
+			@SuppressWarnings("unchecked")
+			List<ConnectorEnd> ends = (List<ConnectorEnd>) ZDLUtil.getValue(objToVerify, ZMLMMNames.ASSEMBLY_CONNECTOR,
+					ZMLMMNames.ASSEMBLY_CONNECTOR__END);
+			EObject connectorContainer = objToVerify.eContainer();
+			if (ends.size() == 2 && ZDLUtil.isZDLConcept(connectorContainer, CCMNames.ASSEMBLY_IMPLEMENTATION)) {
+				ConnectorEnd end1 = ends.get(0);
+				ConnectorEnd end2 = ends.get(1);
+				EObject port1 = end1.getRole();
+				EObject port2 = end2.getRole();
+				if (port1 != null && ZDLUtil.isZDLConcept(port1, CCMNames.INTERFACE_PORT) && port2 != null
+						&& ZDLUtil.isZDLConcept(port2, CCMNames.INTERFACE_PORT)) {
+					if (!validConnectorTypeEnds(end1, end2) && !validConnectorTypeEnds(end2, end1)) {
+						return ctx.createFailureStatus("Invalid AXCIOMA connector ends");
+					}
+				}
+			}
+		}
+
+		return ctx.createSuccessStatus();
 	}
+
+	protected static boolean validConnectorTypeEnds(ConnectorEnd connectorEnd1, ConnectorEnd connectorEnd2) {
+		EObject port1 = connectorEnd1.getRole();
+		EObject port2 = connectorEnd2.getRole();
+		if (!isBorderConnectorEnd(connectorEnd1)) {
+			if (!isBorderConnectorEnd(connectorEnd2)) {
+				if (isReceptacle(port1) && isFacet(port2)
+						&& (hasAMI4CCMConnectorType(port1) || hasCORBA4CCMConnectorType(port1))
+						&& hasCORBA4CCMConnectorType(port2))
+					return true;
+			} else {
+				if (isReceptacle(port1) && isReceptacle(port2)) {
+					if (hasAMI4CCMConnectorType(port1) && hasAMI4CCMConnectorType(port2))
+						return true;
+					if (hasCORBA4CCMConnectorType(port1) && hasCORBA4CCMConnectorType(port2))
+						return true;
+				} else if (isFacet(port1) && isFacet(port2)) {
+					if (hasCORBA4CCMConnectorType(port1) && hasCORBA4CCMConnectorType(port2))
+						return true;
+				}
+			}
+		}
+		return false;
+	}
+
+//	protected static boolean validConnectorTypeEnds(EObject port1, EObject port2) {
+//		if (!isBorderPort(port1)) {
+//			if (!isBorderPort(port2)) {
+//				if (isReceptacle(port1) && isFacet(port2)
+//						&& (hasAMI4CCMConnectorType(port1) || hasCORBA4CCMConnectorType(port1))
+//						&& hasCORBA4CCMConnectorType(port2))
+//					return true;
+//			} else {
+//				if (isReceptacle(port1) && isReceptacle(port2)) {
+//					if (hasAMI4CCMConnectorType(port1) && hasAMI4CCMConnectorType(port2))
+//						return true;
+//					if (hasCORBA4CCMConnectorType(port1) && hasCORBA4CCMConnectorType(port2))
+//						return true;
+//				} else if (isFacet(port1) && isFacet(port2)) {
+//					if (hasCORBA4CCMConnectorType(port1) && hasCORBA4CCMConnectorType(port2))
+//						return true;
+//				}
+//			}
+//		}
+//		return false;
+//	}
+//
+	protected static boolean isBorderConnectorEnd(ConnectorEnd connectorEnd) {
+		return connectorEnd.getPartWithPort() == null;
+	}
+
+	protected static boolean isReceptacle(EObject port) {
+		return (Boolean) ZDLUtil.getValue(port, ZMLMMNames.CONJUGATED_PORT, ZMLMMNames.CONJUGATED_PORT__IS_CONJUGATED);
+	}
+
+	protected static boolean isFacet(EObject port) {
+		return !isReceptacle(port);
+	}
+
+	protected static boolean hasConnectorType(EObject port, ConnectorType connectorType) {
+		EObject portConnectorType = (EObject) ZDLUtil.getValue(port, CCMNames.INTERFACE_PORT,
+				CCMNames.INTERFACE_PORT__CONNECTOR_TYPE);
+		if (portConnectorType instanceof Component) {
+			Component portConnectorTypeComponent = (Component) portConnectorType;
+			return connectorType.name().equals(portConnectorTypeComponent.getName());
+		}
+		return false;
+	}
+
+	protected static boolean hasAMI4CCMConnectorType(EObject port) {
+		return hasConnectorType(port, ConnectorType.AMI4CCM_Connector);
+	}
+
+	protected static boolean hasCORBA4CCMConnectorType(EObject port) {
+		return hasConnectorType(port, ConnectorType.CORBA4CCM_Connector);
+	}
+
 }
